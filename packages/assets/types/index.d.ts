13ca29d21acaf82631538c29503ab731b2e14e11
Dan Lynch <pyramation@gmail.com>
2022-08-14 13:27:15 -0700

    update types

--- packages/assets/types/index.d.ts
+++ packages/assets/types/index.d.ts
@@ -1,11 +1,10 @@
 import assets from './assets';
 import chain from './chain';
 import ibc_assets from './ibc_assets';
-
-export default {
-  chain,
-  assets,
-  ibc_assets
+declare const _default: {
+  chain: Chain;
+  assets: AssetList;
+  ibc_assets: IBCAsset;
 };
-
+export default _default;
 export { assets, chain, ibc_assets };
