44af3a2713071a4326ae538396ebe67194310ac2
Dan Lynch <pyramation@gmail.com>
2022-10-13 14:51:36 -0700

    types

---
+++ packages/juno-network/types/codegen/juno/mint/mint.d.ts
@@ -0,0 +1,44 @@
+import * as _m0 from "protobufjs/minimal";
+import { Long, DeepPartial } from "../../helpers";
+/** Minter represents the minting state. */
+export interface Minter {
+    /** current annual inflation rate */
+    inflation: string;
+    phase: Long;
+    startPhaseBlock: Long;
+    /** current annual expected provisions */
+    annualProvisions: string;
+}
+/** Minter represents the minting state. */
+export interface MinterSDKType {
+    /** current annual inflation rate */
+    inflation: string;
+    phase: Long;
+    start_phase_block: Long;
+    /** current annual expected provisions */
+    annual_provisions: string;
+}
+/** Params holds parameters for the mint module. */
+export interface Params {
+    /** type of coin to mint */
+    mintDenom: string;
+    /** expected blocks per year */
+    blocksPerYear: Long;
+}
+/** Params holds parameters for the mint module. */
+export interface ParamsSDKType {
+    /** type of coin to mint */
+    mint_denom: string;
+    /** expected blocks per year */
+    blocks_per_year: Long;
+}
+export declare const Minter: {
+    encode(message: Minter, writer?: _m0.Writer): _m0.Writer;
+    decode(input: _m0.Reader | Uint8Array, length?: number): Minter;
+    fromPartial(object: DeepPartial<Minter>): Minter;
+};
+export declare const Params: {
+    encode(message: Params, writer?: _m0.Writer): _m0.Writer;
+    decode(input: _m0.Reader | Uint8Array, length?: number): Params;
+    fromPartial(object: DeepPartial<Params>): Params;
+};
